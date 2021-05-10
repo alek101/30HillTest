f40c07ecae10a8dc9d0f165f5be0318a2ce79568
alek101 <47416711+alek101@users.noreply.github.com>
2021-05-10 20:58:17 +0200

    Refactoring 3

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const fs = require('fs');
-const path = 'db/data social graph.json';
 const socialController = require('./controller/SocialController')
-
 
 const app=express();
 
 app.listen(3000);
 
 app.get('/',(req, res) => {
-
     res.send('!')
-
 });
 
 app.get('/directFriends/:id', socialController.directFriends);
