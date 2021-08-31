d2ba392f1049c84eb46dad435d3f443e0e974aaa
alek101 <47416711+alek101@users.noreply.github.com>
2021-09-01 00:00:45 +0200

    Create mongoModelIdea.js

---
+++ mongoModelIdea.js
@@ -0,0 +1,37 @@
+// I Friends
+
+directFriends = await User.find({id: id}).lean().friends;
+
+// II Friends of friends
+
+directFriends = await User.find({id}).lean().friends;
+
+// we want person that have direct firends in his friend list
+// but that person couldn't be one of direct friends, or the begin person
+friendsOfFriends = await User.distinct(id, {
+    friends: { $in: directFriends },
+    id: { $nin: directFriends },
+    id: { $ne: id }
+})
+
+// III Suggested firends
+directFriends = await User.find({id}).lean().friends;
+
+suggestFriends = await User.aggregate([
+    { 
+        $match: {
+            friends: { $in: directFriends },
+            id: { $nin: directFriends },
+            id: { $ne: id }
+        }
+    },
+    {
+        $group: { 
+            _id: '$id',
+            numberOfJoinFriends: { $size: { $setIntersection: [ directFriends, '$friends' ]}}
+        }
+    },
+    {
+        $match: { numberOfJoinFriends: { $gte: 2 } }
+    }
+])
